34556c09e29d1e01f10b21bb8c7b3518bb7a10a6
InDeviceMex <indevicemex@gmail.com>
2019-08-17 09:58:36 -0500

    [LCD1602 4bits Generic] Initial Revision

---
+++ LCD1602 4bits Generic/main.c
@@ -0,0 +1,335 @@
+#include <main.h>
+
+
+void Conf_Reloj(void)
+{
+    DCOCTL=CALDCO_16MHZ;
+    BCSCTL1=CALBC1_16MHZ; //conf para DCO=16MHz aprox 15.62 a 15.72MHz
+
+    /*Seleccion de ACLK*/
+    BCSCTL3&=~(LFXT1S_3);
+    BCSCTL3|=LFXT1S_2;  //ACLK=VLOCLK
+    /*Division de ACLK*/
+    BCSCTL1&=~DIVA_3; //Division de ACLK/1;
+
+    /*SEleccion de MCLK*/
+    BCSCTL2&=~SELM_3; //MCLK=DCOCLK
+    /*Division de MCLK*/
+    BCSCTL2&=~DIVM_3; //Division de MCLK/1;
+
+    /*Seleccion de SMCLK*/
+    BCSCTL2&=~SELS; // 0=DCOCLK 1=ACLK
+    /*Division de SMCLK*/
+    BCSCTL2&=~DIVS_3; //Division de SMCLK/1;
+}
+
+
+void Conf_LCD1602(void)
+{
+    long Delay=0;
+     /*Conf para P1.0=RS P1.1=RW P1.2=E*/
+    E_DIR|=E;
+    E_OUT&=~E;
+    E_REN&=~E;
+    E_SEL&=~E;
+    E_SEL2&=~E;
+
+    RS_DIR |=RS;
+    RS_OUT &=~RS;
+    RS_REN &=~RS;
+    RS_SEL &=~RS;
+    RS_SEL2&=~RS;
+
+    RW_DIR|=RW;
+    RW_OUT&=~RW;
+    RW_REN&=~RW;
+    RW_SEL&=~RW;
+    RW_SEL2&=~RW;
+
+    /*Conf para Datos D4-D7*/
+    D4_DIR|=D4;
+    D4_OUT&=~D4;
+    D4_REN&=~D4;
+    D4_SEL&=~D4;
+    D4_SEL2&=~D4;
+
+    D5_DIR|=D5;
+    D5_OUT&=~D5;
+    D5_REN&=~D5;
+    D5_SEL&=~D5;
+    D5_SEL2&=~D5;
+
+    D6_DIR|=D6;
+    D6_OUT&=~D6;
+    D6_REN&=~D6;
+    D6_SEL&=~D6;
+    D6_SEL2&=~D6;
+
+    D7_DIR|=D7;
+    D7_OUT&=~D7;
+    D7_REN&=~D7;
+    D7_SEL&=~D7;
+    D7_SEL2&=~D7;
+
+    for(Delay=30000; Delay>0; Delay--);//20 ms aprox a 16MHz
+    //manda 0x3 en la parte alta de la LCD
+    D4_OUT|=D4;
+    D5_OUT|=D5;
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+    for(Delay=10000; Delay>0; Delay--);//7 ms aprox a 16MHz
+
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+    for(Delay=500; Delay>0; Delay--);//100 us aprox a 16MHz
+
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+    for(Delay=10000; Delay>0; Delay--);//7 ms aprox a 16MHz
+
+    //primer comando 0x20 configuracion a 4 bits
+    D4_OUT&=~D4; //conf para los pines D4-7 = P1.4-7
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+    for(Delay=240; Delay>0; Delay--);//40 us aprox a 16MHz
+
+    /*M8BIT o M4BIT,M2LINE o M1LINE, M5_8_Font o M5_11_Font */
+    LCD1602_Com(M4BIT|M2LINE|M5_8_Font);
+
+    /*ONDisp o OFFDisp, ONCursor o OFFCursor,ONBlink o OFFBLink*/
+    LCD1602_Com(ONDisp|OFFCursor|OFFBlink );
+
+    LCD1602_Com(CLEAR); //limpia la pantalla
+    for(Delay=3000; Delay>0; Delay--);//1.60 ms aprox a 16MHz
+
+    /*IAdd o DAdd*/
+    LCD1602_Com(IncAdd);
+
+    LCD1602_Com(HOME);//Manda cursor a home
+    for(Delay=3000; Delay>0; Delay--);//1.60 ms aprox a 16MHz
+}
+
+
+void LCD1602_Com(char comando)
+{
+    unsigned char Delay=0;
+    unsigned char valorD4=0,valorD5=0,valorD6=0,valorD7=0;
+    unsigned char valorRW=0, valorRS=0;
+    //guarda los valores anterior de los pines, en caso que se compartan con otras cosas
+    valorD4=D4_IN & D4;
+    valorD5=D5_IN & D5;
+    valorD6=D6_IN & D6;
+    valorD7=D7_IN & D7;
+    valorRW=RW_IN & RW;
+    valorRS=RS_IN & RS;
+
+    RW_OUT&=~RW; //RW para escritura 1=lectura 0=escritura
+    RS_OUT&=~RS; //RS 1=dato 0=comando
+
+
+    //D7_OUT&=~D7;
+    //D7_OUT|=((comando&0x80)&&1)<<D7_POS;
+    //D6_OUT&=~D6;
+    //D6_OUT|=((comando&0x40)&&1)<<D6_POS;
+    //D5_OUT&=~D5;
+    //D5_OUT|=((comando&0x20)&&1)<<D5_POS;
+    //D4_OUT&=~D4;
+    //D4_OUT|=((comando&0x10)&&1)<<D4_POS;
+    if(comando&0x80)
+        D7_OUT|=D7;
+    else
+        D7_OUT&=~D7;
+
+    if(comando&0x40)
+        D6_OUT|=D6;
+    else
+        D6_OUT&=~D6;
+
+    if(comando&0x20)
+        D5_OUT|=D5;
+    else
+        D5_OUT&=~D5;
+
+    if(comando&0x10)
+        D4_OUT|=D4;
+    else
+        D4_OUT&=~D4;
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+
+    //D7_OUT&=~D7;
+    //D7_OUT|=((comando&0x8)&&1)<<D7_POS;
+    //D6_OUT&=~D6;
+    //D6_OUT|=((comando&0x4)&&1)<<D6_POS;
+    //D5_OUT&=~D5;
+    //D5_OUT|=((comando&0x2)&&1)<<D5_POS;
+    //D4_OUT&=~D4;
+    //D4_OUT|=((comando&0x1)&&1)<<D4_POS;
+
+    if(comando&0x8)
+        D7_OUT|=D7;
+    else
+        D7_OUT&=~D7;
+
+    if(comando&0x4)
+        D6_OUT|=D6;
+    else
+        D6_OUT&=~D6;
+
+    if(comando&0x2)
+        D5_OUT|=D5;
+    else
+        D5_OUT&=~D5;
+
+    if(comando&0x1)
+        D4_OUT|=D4;
+    else
+        D4_OUT&=~D4;
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+    for(Delay=150; Delay>0; Delay--);//40 us aprox a 16MHz
+
+
+    D4_OUT&=~D4;//reinicia los bits
+    D5_OUT&=~D5;
+    D6_OUT&=~D6;
+    D7_OUT&=~D7;
+    RS_OUT&=~RS;
+    RW_OUT&=~RW;
+    D4_OUT|=valorD4; //restaura el valor anterior
+    D5_OUT|=valorD5;
+    D6_OUT|=valorD6;
+    D7_OUT|=valorD7;
+    RS_OUT|=valorRS;
+    RW_OUT|=valorRW;
+}
+void LCD1602_Dato(char dato)
+{
+    unsigned char Delay=0;
+    unsigned char valorD4=0,valorD5=0,valorD6=0,valorD7=0;
+    unsigned char valorRW=0, valorRS=0;
+    //guarda los valores anterior de los pines, en caso que se compartan con otras cosas
+    valorD4=D4_IN & D4;
+    valorD5=D5_IN & D5;
+    valorD6=D6_IN & D6;
+    valorD7=D7_IN & D7;
+    valorRW=RW_IN & RW;
+    valorRS=RS_IN & RS;
+
+    RW_OUT&=~RW; //RW para escritura 1=lectura 0=escritura
+    RS_OUT|=RS; //RS 1=dato 0=comando
+
+
+    //D7_OUT&=~D7;
+    //D7_OUT|=((dato&0x80)&&1)<<D7_POS;
+    //D6_OUT&=~D6;
+    //D6_OUT|=((dato&0x40)&&1)<<D6_POS;
+    //D5_OUT&=~D5;
+    //D5_OUT|=((dato&0x20)&&1)<<D5_POS;
+    //D4_OUT&=~D4;
+    //D4_OUT|=((dato&0x10)&&1)<<D4_POS;
+    if(dato&0x80)
+        D7_OUT|=D7;
+    else
+        D7_OUT&=~D7;
+
+    if(dato&0x40)
+        D6_OUT|=D6;
+    else
+        D6_OUT&=~D6;
+
+    if(dato&0x20)
+        D5_OUT|=D5;
+    else
+        D5_OUT&=~D5;
+
+    if(dato&0x10)
+        D4_OUT|=D4;
+    else
+        D4_OUT&=~D4;
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+
+    //D7_OUT&=~D7;
+    //D7_OUT|=((dato&0x8)&&1)<<D7_POS;
+    //D6_OUT&=~D6;
+    //D6_OUT|=((dato&0x4)&&1)<<D6_POS;
+    //D5_OUT&=~D5;
+    //D5_OUT|=((dato&0x2)&&1)<<D5_POS;
+    //D4_OUT&=~D4;
+    //D4_OUT|=((dato&0x1)&&1)<<D4_POS;
+
+    if(dato&0x8)
+        D7_OUT|=D7;
+    else
+        D7_OUT&=~D7;
+
+    if(dato&0x4)
+        D6_OUT|=D6;
+    else
+        D6_OUT&=~D6;
+
+    if(dato&0x2)
+        D5_OUT|=D5;
+    else
+        D5_OUT&=~D5;
+
+    if(dato&0x1)
+        D4_OUT|=D4;
+    else
+        D4_OUT&=~D4;
+    LCD1602_E(); //manda un pulso en el pin E (Enable)
+    for(Delay=150; Delay>0; Delay--);//40 us aprox a 16MHz
+
+
+    D4_OUT&=~D4;//reinicia los bits
+    D5_OUT&=~D5;
+    D6_OUT&=~D6;
+    D7_OUT&=~D7;
+    RS_OUT&=~RS;
+    RW_OUT&=~RW;
+    D4_OUT|=valorD4; //restaura el valor anterior
+    D5_OUT|=valorD5;
+    D6_OUT|=valorD6;
+    D7_OUT|=valorD7;
+    RS_OUT|=valorRS;
+    RW_OUT|=valorRW;
+}
+void LCD1602_E(void)
+{
+    E_OUT|=E; //manda a 1 logico el bit E (Enable)
+    E_OUT&=~E;//manda a 0 logico E, completando el pulso
+}
+
+void LCD1602_Pos(char columna, char fila)
+{
+    long direccion=0x80;
+    direccion|=columna&0xF;
+    direccion|=((fila&0x1)<<6);
+    LCD1602_Com(direccion);
+}
+
+/**
+ * main.c
+ */
+void main(void)
+{
+       WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+       Conf_Reloj(); //conf de reloj 16 MHz, SMCLK y MCLK
+       //4 bits , 2 lineas, 5x8 puntos, incr cursor
+       Conf_LCD1602();//configuracion de la pantalla LCD1602
+       LCD1602_Pos(3,0);
+       LCD1602_Dato('H');
+       LCD1602_Dato('O');
+       LCD1602_Dato('L');
+       LCD1602_Dato('A');
+       LCD1602_Dato(' ');
+       LCD1602_Dato('M');
+       LCD1602_Dato('U');
+       LCD1602_Dato('N');
+       LCD1602_Dato('D');
+       LCD1602_Dato('O');
+       LCD1602_Pos(4,1);
+       LCD1602_Dato('I'); //escribe H en 0,0. Cursor 0,1
+       LCD1602_Dato('n');
+       LCD1602_Dato('D'); //escribe H en 0,0. Cursor 0,1
+       LCD1602_Dato('e');
+       LCD1602_Dato('v');
+       LCD1602_Dato('i');
+       LCD1602_Dato('c');
+       LCD1602_Dato('e');
+       while(1);
+}
---
+++ LCD1602 4bits Generic/main.h
@@ -0,0 +1,167 @@
+/*
+ * main.h
+ *
+ *  Created on: 23/08/2017
+ *      Author: Lalo
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "msp430g2553.h"
+
+
+void Conf_Reloj(void);
+void Conf_LCD1602(void);
+void LCD1602_Com(char comando);
+void LCD1602_Dato(char dato);
+void LCD1602_E(void);
+void LCD1602_Pos(char columna, char fila);
+
+/*DEfinciones de Comando*/
+#define CLEAR 0x1 //limpia la pantalla
+
+#define HOME  0x2 //manda cursor a Home 0,0
+
+//Combinacion de los siguientes parametros para mandar comando
+#define IncAdd  0x6 //Incrementa la direccion en cada escritura
+#define DecAdd  0x4 //Decrementa la direccion en cada escritura
+
+#define NSDisp 0x4 //no desplaza la pantalla
+#define SDisp 0x5 //desplaza la pantalla 1 posicion
+
+//combinacion de los siguientes parametros para mandar comando
+#define ONDisp    0xC //enciende la pantalla
+#define OFFDisp   0x8 //apaga la pantalla
+
+#define ONCursor  0xA //prende el cursor visual
+#define OFFCursor 0x8 //apaga el cursor visual
+
+#define ONBlink   0x9 //hace que el cursor parpadee
+#define OFFBlink  0x8 //El parpadeo se desactiva
+
+//mandar solo una defincion en este comando
+#define SRDisp  0x1C //Desplaza la pantalla a la der, la dir no cambia
+#define SLDisp  0x18 //Desplaza la pantalla a la izq, la dir no cambia
+#define CRDisp  0x14 //mueve el cursor a la der, la dir aumenta en 1
+#define CLDisp  0x10 //mueve el cursor a la izq, la dir disminuye en 1
+
+//combinacion de los siguientes parametros para mandar comando
+#define M8BIT     0x30 //modo 8bit
+#define M4BIT     0x20 //modo 4bit
+
+#define M1LINE    0x20 //modo 1 linea
+#define M2LINE    0x28 //modo 2 lineas
+
+#define M5_8_Font 0x20 //fuente 5x8
+#define M5_11_Font 0x24 //fuente 5x11 solo para 1 linea
+
+#define PUERTO_E  P1
+#define PUERTO_RS P1
+#define PUERTO_RW P1
+#define PUERTO_D4 P1
+#define PUERTO_D5 P1
+#define PUERTO_D6 P1
+#define PUERTO_D7 P1
+
+#define E  BIT2
+#define RS BIT0
+#define RW BIT1
+#define D4 BIT7
+#define D5 BIT4
+#define D6 BIT5
+#define D7 BIT6
+
+#define E_POS  2
+#define RS_POS 0
+#define RW_POS 1
+#define D4_POS 7
+#define D5_POS 4
+#define D6_POS 5
+#define D7_POS 6
+
+
+#define OUTp(p)    Out(p)
+#define Out(p)    p ## OUT    //abreviacion para seleccionar el puerto indicado (PxOUT)
+
+#define DIRp(p)    Dir(p)
+#define Dir(p)    p ## DIR    //abreviacion para seleccionar el puerto indicado (PxDIR)
+
+#define INp(p)     In(p)
+#define In(p)     p ## IN     //abreviacion para seleccionar el puerto indicado (PxIN)
+
+#define SELp(p)    Sel(p)
+#define Sel(p)    p ## SEL    //abreviacion para seleccionar el puerto indicado (PxSEL)
+
+#define RENp(p)    Ren(p)
+#define Ren(p)    p ## REN    //abreviacion para seleccionar el puerto indicado (PxREN)
+
+#define SEL2p(p)   Sel2(p)
+#define Sel2(p)   p##SEL2
+
+
+
+//------------------ Definiciones Pin E ---------------
+
+#define E_OUT   OUTp( PUERTO_E )
+#define E_DIR   DIRp(PUERTO_E)
+#define E_REN   RENp( PUERTO_E )
+#define E_SEL   SELp(PUERTO_E)
+#define E_SEL2  SEL2p(PUERTO_E)
+#define E_IN    INp(PUERTO_E)
+
+//------------------ Definiciones Pin RS ---------------
+
+#define RS_OUT   OUTp( PUERTO_RS )
+#define RS_DIR   DIRp(PUERTO_RS)
+#define RS_REN   RENp( PUERTO_RS )
+#define RS_SEL   SELp(PUERTO_RS)
+#define RS_SEL2  SEL2p(PUERTO_RS)
+#define RS_IN    INp(PUERTO_RS)
+
+//------------------ Definiciones Pin RW ---------------
+
+#define RW_OUT   OUTp( PUERTO_RW )
+#define RW_DIR   DIRp(PUERTO_RW)
+#define RW_REN   RENp( PUERTO_RW )
+#define RW_SEL   SELp(PUERTO_RW)
+#define RW_SEL2  SEL2p(PUERTO_RW)
+#define RW_IN    INp(PUERTO_RW)
+
+//------------------ Definiciones Pin D4 ---------------
+
+#define D4_OUT   OUTp( PUERTO_D4 )
+#define D4_DIR   DIRp(PUERTO_D4)
+#define D4_REN   RENp( PUERTO_D4 )
+#define D4_SEL   SELp(PUERTO_D4)
+#define D4_SEL2  SEL2p(PUERTO_D4)
+#define D4_IN    INp(PUERTO_D4)
+
+//------------------ Definiciones Pin D5 ---------------
+
+#define D5_OUT   OUTp( PUERTO_D5 )
+#define D5_DIR   DIRp(PUERTO_D5)
+#define D5_REN   RENp( PUERTO_D5 )
+#define D5_SEL   SELp(PUERTO_D5)
+#define D5_SEL2  SEL2p(PUERTO_D5)
+#define D5_IN    INp(PUERTO_D5)
+
+//------------------ Definiciones Pin D6 ---------------
+
+#define D6_OUT   OUTp( PUERTO_D6 )
+#define D6_DIR   DIRp(PUERTO_D6)
+#define D6_REN   RENp( PUERTO_D6 )
+#define D6_SEL   SELp(PUERTO_D6)
+#define D6_SEL2  SEL2p(PUERTO_D6)
+#define D6_IN    INp(PUERTO_D6)
+//------------------ Definiciones Pin D7 ---------------
+
+#define D7_OUT   OUTp( PUERTO_D7 )
+#define D7_DIR   DIRp(PUERTO_D7)
+#define D7_REN   RENp( PUERTO_D7 )
+#define D7_SEL   SELp(PUERTO_D7)
+#define D7_SEL2  SEL2p(PUERTO_D7)
+#define D7_IN    INp(PUERTO_D7)
+
+
+#endif /* MAIN_H_ */
