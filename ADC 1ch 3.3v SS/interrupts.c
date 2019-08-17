f8f7452da4f51bb36661e24b382e68f3eb59c7fa
InDeviceMex <indevicemex@gmail.com>
2019-08-17 12:30:33 -0500

    [ADC 1ch 3.3v SS] Added flag

--- ADC 1ch 3.3v SS/interrupts.c
+++ ADC 1ch 3.3v SS/interrupts.c
@@ -10,11 +10,11 @@
 #include "interrupts.h"
 
 uint16_t u16ValorADC=0;
-//uint16_t u16flagADC=0;
+uint16_t u16flagADC=0;
 
 #pragma vector=ADC10_VECTOR;
 __interrupt void ADC_vIsr(void)
 {
     u16ValorADC=ADC10MEM;
-    //u16flagADC=0;
+    u16flagADC=0;
 }
